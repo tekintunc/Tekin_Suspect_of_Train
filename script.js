7791ccc78a46aed078235fdc3bfe52351a759fae
tekintunc <115110134+tekintunc@users.noreply.github.com>
2022-12-10 01:29:16 +0300

    düzenlemeler yapıldı

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-random= Math.floor(Math.random()*10)+1;
- 
-while(1)
-{
-  sayi= prompt('Sence suçlu 10 vagonlu bu trende hangi vagonda ?');
-   if (sayi>random)
-   alert('Daha aşağıdaki vagonlardan birinde');
+// Önce 1'den 10'a kadar rastgele bir sayı üret
 
-   else if (sayi<random)
-   alert('Daha yukarıdaki vagonlardan birinde');
+var random = Math.floor(Math.random() * 10) + 1;
+var hak = 4;
 
-   else if (sayi==random){
-  alert('Bravo, Suçluyu yakaladınız'); break;
+for (var tahminsayisi = 1; tahminsayisi <= hak; tahminsayisi++) {
+  var tahmin = parseInt(prompt("10 vagonlu bu trende şüpheli sence hangi vagonda ?"));
+
+  if (random == tahmin) {
+    alert(
+      `Bravo ${tahminsayisi} defada  şüphelinin hangi vagonda saklandığını buldun`
+    );
+
+    alert(`Kazandığın puan: ${100 - 25 * (tahminsayisi - 1)}`);
+  } else if (random > tahmin) {
+    alert("Daha Büyük bir sayı giriniz,Çünkü şüpheli yukarıdaki vagonlardan birinde");
+  } else {
+    alert("Daha Kücük bir sayı giriniz, Çünkü şüpheli aşağıdaki vagonlardan birinde");
+  }
+  if (tahminsayisi == hak) {
+    alert(" Tahmin hakkınız bitti Şüpheliyi bulamadınız");
+  }
 }
-
-}
- 
